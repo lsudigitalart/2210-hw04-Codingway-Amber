8aa3a24c394b0411dcf529b915c3e171a7ac4af5
Codingway-Amber <amberreine7@gmail.com>
2024-10-02 13:09:00 -0500

    added new square

--- sketch.js
+++ sketch.js
@@ -1,23 +1,14 @@
+// var x = 10;
+
 function setup() {
     createCanvas (600, 600); 
     noCursor(); 
-    strokeWeight(20);
+    
 } 
 
 function draw() { 
-    background(100);
-    fill(255)
-    stroke(102)
-    ellipse(mouseX, mouseY, 50); 
-
-    if(mouseX > 200) {
-        if(mouseY > 200) {
-        fill(255, 200, 200);
-     }
-    }
-    square(200, 200, 200); 
-    if (mouseIsPressed == true) { 
-        stroke(0)
-    } 
-    line(0, 70, width, 50);
-}
+    background(100); 
+    fill(255, 178, 202);
+    square(mouseX, mouseY, 55, 20); 
+    
+} 
