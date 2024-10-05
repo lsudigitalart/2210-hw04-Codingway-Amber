bffc4613f50b52ccf12c8e5547642288901ae31b
Codingway-Amber <amberreine7@gmail.com>
2024-10-05 15:37:20 -0500

    added key movements

--- sketch.js
+++ sketch.js
@@ -1,14 +1,29 @@
-// var x = 10;
+let x, y;
+let speed = 5;
 
 function setup() {
-    createCanvas (600, 600); 
-    noCursor(); 
-    
-} 
+  createCanvas(600, 600);
+  noCursor();
+  x = width / 2;
+  y = height / 2;
+}
 
-function draw() { 
-    background(100); 
-    fill(255, 178, 202);
-    square(mouseX, mouseY, 55, 20); 
-    
-} 
+function draw() {
+  background(100);
+  fill(255, 178, 202);
+  square(x, y, 55, 20);
+
+  // Move square with WASD keys
+  if (keyIsDown(87)) { // W key
+    y -= speed;
+  }
+  if (keyIsDown(83)) { // S key
+    y += speed;
+  }
+  if (keyIsDown(65)) { // A key
+    x -= speed;
+  }
+  if (keyIsDown(68)) { // D key
+    x += speed;
+  }
+}
